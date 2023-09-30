c21c912701f5dc827161c0dc09737f98d6318e72
fabiobessa <fabiobessa97@gmail.com>
2023-09-30 14:49:36 -0300

    Consome componente de paginação

--- src/components/CharactersSection.vue
+++ src/components/CharactersSection.vue
@@ -1,15 +1,21 @@
 <template>
   <section class="container-md py-md-5">
-    <div class="row mx-4">
+    <div class="row">
       <div class="col-md-12 col-xl-6 d-xl-block d-flex justify-content-center my-2" v-for="character in characterListData" :key="character.id" >
         <CharacterCard :character="character"/>
       </div>
     </div>
+    <Pagination
+      v-if="paginationCharacter && paginationCharacter.pages > 1"
+      :pagination="paginationCharacter"
+      @to-page="handlePagination"
+    />
   </section>
 </template>
 
 <script setup lang="ts">
   import CharacterCard from '@/components/CharacterCard.vue'
+  import Pagination from '@/components/Pagination.vue'
   import { onMounted } from "vue"
   import { useCharacterStore } from '@/stores/character'
   import { storeToRefs } from "pinia";
@@ -18,6 +24,7 @@
   const { loadCharacters } = characterStore
   const {
     searchStateCharacter,
+    searchQueryCharacter,
     characterListData,
     paginationCharacter
   } = storeToRefs(characterStore)
@@ -25,4 +32,9 @@
   onMounted(async () => {
     await loadCharacters()
   })
+
+  function handlePagination(page: number) {
+    searchQueryCharacter.value.page = page
+    loadCharacters()
+  }
 </script>
